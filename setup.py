7c651459ac4d52a8d6850efdc38af9ca3e7ff903
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-04-06 22:17:58 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from idevision import __version__ as version
+
+with open("README.md", "r", encoding="utf-8") as fp:
+  long_desc = fp.read()
+  
+setuptools.setup(
+  name = "idevision",
+  version = version,
+  author = "proguy914629",
+  description = "An async wrapper for the IDevision API",
+  long_description = long_descm
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/proguy914629bot/async-idevision",
+  python_requires = ">=3.6",
+  install_requires = ["aiohttp"]
+)
