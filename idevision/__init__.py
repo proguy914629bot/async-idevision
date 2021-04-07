dd6d971f4e49385622961a2cc3511ef23bb5a20e
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-04-07 10:24:32 +0700

    Updated __init__.py file.
    Added an Exception for when you are trying to access an endpoint without an API Token.

--- idevision/__init__.py
+++ idevision/__init__.py
@@ -35,13 +35,47 @@ __version__ = "0.9.1"
 baseURL = "https://idevision.net"
 Token = ""
 
+class IDevisionException(Exception):
+  """A Global Exception for the IDevision API."""
+
+class BadAuthorization(IDevisionException):
+  """An Exception when you are trying to access an API with bad authorization (No Token, Token Invalid, etc). This is a subclass of idevision.IDevisionException"""
+  
+class Ratelimit(IDevisionException):
+  """An Exception when you are Ratelimited. This is a subclass of idevision.IDevisionException"""
+  
+class Banned(Ratelimit):
+  """An Exception when you are Banned from the API. This is a subclass of idevision.Ratelimit."""
+  
+class BadRequest(IDevisionException):
+  """An Exception when the API has a Bad Request. This is a subclass of idevision.IDevisionException"""
+  
+class MissingRequiredArgument(IDevisionException):
+  """An Exception when a """
+
 class IDevision:
   def __init__(self, token : str = ""):
+    """
+    Async IDevision API Class.
+    
+    Parameters
+    -----------
+    token: :class:`str`
+        The token for your IDevision API to access specific Endpoints.
+    """
     self.token = token
+    self.documentation = "https://idevision.net/docs"
+    self.docs = "https://idevision.net/docs"
+    self.baseURL = basseURL
     Token = token
     
   class rtfs:
     async def __init__(self, query : str, library : str, *, format : str = "links"):
+      """
+      |coro|
+      
+      A helper for the RTFS API.
+      """
       library = find(lambda m: library.lower() in m.lower(), ["discord.py", "twitchio", "wavelink", "aiohttp"])
       format = find(lambda m: format.lower() in m.lower(), ["links", "source"])
       params = {
@@ -60,11 +94,19 @@ class IDevision:
       self.js = await r.json()
           
     @property
-    def nodes(self):
+    def nodes(self) -> dict:
+      """
+      Returns a dict of the node name and URL/Source. 
+      The dict will be in the format {"Node Name": "URL/Source of Documentation."}
+      """
       return self.js["nodes"]
     
     @property
-    def query_time(self):
+    def query_time(self) -> str:
+      '''
+      Returns a string of the query time. E.g "1.0".
+      '''
       return self.js["query_time"]
-    
   
+  #class rtfm:
+    #async def __init__(self, query : str)
