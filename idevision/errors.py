67f1871b8a244380cb40e148cc31ce75243d1db1
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-04-27 14:13:09 +0700

    Update errors.py

--- idevision/errors.py
+++ idevision/errors.py
@@ -23,40 +23,46 @@ SOFTWARE.
 """
 
 errorCodes = {
-  "503": "Service Unavailable",
-  "500": "Internal Server Error",
-  "200": "Ok.",
-  "400": "Bad Request.",
-  "401": "Bad Authorization/Unauthorized.",
-  "404": "Not found.",
-  "403": "Banned.",
-  "429": "Ratelimited.",
-  "201": "Ok.",
-  "204": "Ok."
+  "503": ("Service Unavailable", ServiceUnavailable),
+  "500": ("Internal Server Error", InternalServerError),
+  "200": ("Ok.", None),
+  "400": ("Bad Request.", BadRequest),
+  "401": ("Bad Authorization/Unauthorized.", BadAuthorization),
+  "404": ("Not found.", NotFound),
+  "403": ("Banned.", Banned),
+  "429": ("Ratelimited.", Ratelimit),
+  "201": ("Ok.", None),
+  "204": ("Ok.", None)
 }
 
 class IDevisionException(Exception):
   """A Global Exception for the IDevision API."""
   def __init__(self, *args, **kwargs):
     return super().__init__(*args, **kwargs)
+  
+class InternalServerError(IDevisionException):
+  """An Exception when the IDevison API returns a 503 Status Code."""
+  def __init__(self):
+    super().__init__("API Returned a 500 Status Code.")
 
 class BadAuthorization(IDevisionException):
   """An Exception when you are trying to access an API with bad authorization (No Token, Token Invalid, etc). This is a subclass of idevision.IDevisionException"""
-  def __init__(self, endpoint):
+  def __init__(self, endpoint = None):
+    endpoint = endpoint or ""
     if len(Token) == 0:
-      return super().__init__("A token is required to access the {} endpoint.")
+      super().__init__("A token is required to access the %s endpoint." % endpoint)
     else:
-      return super().__init__("You have passed an invalid token. Your current passed token is {}. You will not be able to access any other required token endpoints such as CDN and OCR if you do not change the current token passed.")
+      super().__init__("Invalid Token.")
   
 class Ratelimit(IDevisionException):
   """An Exception when you are Ratelimited. This is a subclass of idevision.IDevisionException"""
   def __init__(self):
-    return super().__init__("You are currently ratelimited from IDevision API. You have recieved a 429 error code. Please follow the Ratelimit rules of IDevision from now on or you might get banned.")
+    super().__init__("You are currently ratelimited from IDevision API. You have recieved a 429 error code. Please follow the Ratelimit rules of IDevision from now on or you might get banned.")
   
 class Banned(Ratelimit):
   """An Exception when you are Banned from the API. This is a subclass of idevision.Ratelimit."""
   def __init__(self):
-    return super().__init__("You are banned from IDevision API. You have recieved a 403 error code.")
+    super().__init__("You are banned from IDevision API. You have recieved a 403 error code.")
   
 class BadRequest(IDevisionException):
   """An Exception when the API has raised a Bad Request. This is a subclass of idevision.IDevisionException"""
@@ -64,18 +70,17 @@ class BadRequest(IDevisionException):
 class BadArgument(IDevisionException):
   """An Exception when there is a bad argument to a parameter. This is a subclass of idevision.IDevisionException"""
   def __init__(self, argument, expectedVal, inputVal):
-    return super().__init__(
-      "A bad argument was raised in parameter {}. Expected value {} but got {} instead.".format(
-        str(argument), str(expectedVal), str(inputVal)
+    super().__init__(
+      "A bad argument was raised in parameter %s. Expected value %u but got %s instead." % (argument, expectedVal, inputVal)
       )
     )
   
 class ServiceUnavailable(IDevisionException):
   """An Exception when the IDevison API returns a 503 Status Code."""
   def __init__(self):
-    return super().__init__("API Returned a 503 Status Code.")
+    super().__init__("API Returned a 503 Status Code.")
     
 class NotFound(IDevisionException):
     """An Exception when the IDevision API returns a 404 Status Code."""
-    def __init__(self):
-      return super().__init__("API Returned a 404 Status Code.")
+    def __init__(self, url : str):
+      super().__init__("URL %s Not Found. API Returned a 404 Status Code." % url)
